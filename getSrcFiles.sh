bb27ed5395f34eefbad11badaed0906bc77818e9
Rachel Church <rachel.church@clover.com>
2020-09-22 10:47:22 -0700

    v1 of slides are complete

---
+++ getSrcFiles.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+mkdir -p src
+mkdir -p src/step0
+mkdir -p src/step1
+mkdir -p src/step2
+mkdir -p src/step3
+mkdir -p src/step4
+mkdir -p src/step5
+mkdir -p src/step6
+mkdir -p src/step7
+
+base_url="https://raw.githubusercontent.com/rachel-church/summer-of-qraphql"
+
+curl $base_url/master/src/App.jsx -o src/step0/App.jsx
+
+curl $base_url/step1/addTypescript/src/App.tsx -o src/step1/App.tsx
+
+curl $base_url/step2/addCodegen/src/App.tsx -o src/step2/App.tsx
+curl $base_url/step2/addCodegen/codegen.yml -o src/step2/codegen.yml
+curl $base_url/step2/addCodegen/src/schema.ts -o src/step2/schema.ts
+curl $base_url/step2/addCodegen/src/index.tsx -o src/step2/index.tsx
+
+curl $base_url/step3/addApollo/src/App.tsx -o src/step3/App.tsx
+curl $base_url/step3/addApollo/src/schema.ts -o src/step3/schema.ts
+curl $base_url/step3/addApollo/src/index.tsx -o src/step3/index.tsx
+
+curl $base_url/step4/addVariable/src/App.tsx -o src/step4/App.tsx
+
+curl $base_url/step5/addOperationsPlugin/src/App.tsx -o src/step5/App.tsx
+curl $base_url/step5/addOperationsPlugin/codegen.yml -o src/step5/codegen.yml
+
+curl $base_url/step6/addApolloPlugin/src/App.tsx -o src/step6/App.tsx
+curl $base_url/step6/addApolloPlugin/codegen.yml -o src/step6/codegen.yml
+
+curl $base_url/step7/useGraphqlFile/codegen.yml -o src/step7/codegen.yml
---
+++ src/step4/App.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { DogToy, DogToyCollection } from './schema';
+import { useQuery, gql } from '@apollo/client';
+
+import './App.css';
+
+// Utilize the environment variables defined in the `.env` file
+const {
+  REACT_APP_CONTENTFUL_SPACE_ID: SPACE_ID,
+  REACT_APP_CONTENTFUL_ENVIRONMENT: ENVIRONMENT,
+  REACT_APP_CONTENTFUL_API_KEY: API_KEY,
+} = process.env;
+
+const CONTENTFUL_GRAPHQL_ENDPOINT = `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}/environments/${ENVIRONMENT}`;
+
+// `gql` is a template literal tag that parses GraphQL query strings into the standard GraphQL AST (abstract syntax tree).
+// Allows Apollo to be smarter because it can traverse the query to perform validation and optimizations.
+const GET_TOYS_QUERY = gql`
+  query ($maxPrice: Float) {
+    dogToyCollection(where: {estimatedPrice_lt: $maxPrice}) {
+      items {
+        sys { id }
+        toyName
+        estimatedPrice
+        toyPhoto { url }
+      }
+    }
+  }
+`
+
+/**
+ * TS type mirroring the structure of our graphQL query
+ */
+type DogToyResponse = {
+  dogToyCollection: DogToyCollection
+}
+
+type DogToyVariables = {
+  maxPrice: number
+}
+
+const App = () => {
+  const { loading, error, data } = useQuery<DogToyResponse, DogToyVariables>(GET_TOYS_QUERY, {
+    variables: {
+      maxPrice: 20
+    }
+  });
+
+  // Use optional chaining to retrieve the nested items array OR default to an empty array
+  const allToys = (data?.dogToyCollection?.items || []) as DogToy[];
+
+  if (error) {
+    console.error(error);
+    return <div className="app"><h1>Oops! Something went wrong</h1></div>
+  } else if (loading) {
+    return <div className="app">
+      <div className="loader"/>
+    </div>;
+  }
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1>Welcome! These are Cowboy's toys</h1>
+        <ul className="productList">
+          {allToys.map((toy) =>
+            <li key={toy.sys.id} id={toy.sys.id} className="productCard">
+              <h2>
+                <span>{toy.toyName}</span>
+                <span>${toy.estimatedPrice}</span>
+              </h2>
+              {toy.toyPhoto?.url && <img src={toy.toyPhoto.url} alt=""/>}
+            </li>,
+          )}
+        </ul>
+      </div>
+      <footer>
+        <div className="container">
+          The GraphQL Schema for the above data can be explored within <a
+          href={`${CONTENTFUL_GRAPHQL_ENDPOINT}/explore?access_token=${API_KEY}`}>GraphiQL</a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
